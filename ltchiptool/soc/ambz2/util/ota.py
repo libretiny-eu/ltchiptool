d272762e9638872bb0b059c20e09770cdaeb56c8
Martin Prokopič <martin@prokopic.net>
2024-12-03 21:48:26 +0100

    [ambz2] Fix CI

--- ltchiptool/soc/ambz2/util/ota.py
+++ ltchiptool/soc/ambz2/util/ota.py
@@ -1,10 +1,10 @@
 #  Copyright (c) Martin Prokopič 2024-12-02
 
-from .models.images import IMAGE_PUBLIC_KEY_OFFSET
+from .models.images import IMAGE_PUBLIC_KEY_OFFSET, IMAGE_SIGNATURE_OFFSET
 
 
 def patch_firmware_for_ota(data: bytes) -> bytes:
     copy = bytearray(data)
-    copy[0] ^= 0xff  # negate first signature byte
-    copy[IMAGE_PUBLIC_KEY_OFFSET] ^= 0xff  # negate first pubkey byte
+    copy[IMAGE_SIGNATURE_OFFSET] ^= 0xFF  # negate first signature byte
+    copy[IMAGE_PUBLIC_KEY_OFFSET] ^= 0xFF  # negate first pubkey byte
     return bytes(copy)
